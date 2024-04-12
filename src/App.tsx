2051db96806b0e05eee0566518d9d6b2a6ade8f9
gjeodnd12165 <princewoong@gmail.com>
2024-04-12 16:45:09 +0900

    Revert "create handler.ts"
    This reverts commit 27882628823dbe3c9dbcc6063eb9c7275ce027f9.

--- src/App.tsx
+++ src/App.tsx
@@ -31,9 +31,6 @@ function App() {
   const [hoveredCell, setHoveredCell] = useState<number | null>(null);
 
   const [mines, setMines] = useState<number[]>([]);
-  const calcNewMines = () => {
-    return data ? _.sampleSize(_.range(data.length), data.length / mineRate) : [];
-  }
   const [isGameOver, setIsGameOver] = useState<boolean>(false);
 
 
@@ -41,7 +38,7 @@ function App() {
     setRevealedCells([]);
     setFlaggedCells([]);
     setHoveredCell(null);
-    setMines(calcNewMines());
+    setMines(data ? _.sampleSize(_.range(data.length), data.length / mineRate) : []);
   }, [data]); // data가 재 로딩 되었을 때 실행되는 함수
   const names: string[] = useMemo(() => {
     return data?.map((d) => d.name) ?? ["nodes not found"];
@@ -65,21 +62,80 @@ function App() {
     return _.isEqual(mines.sort(), flaggedCells.sort());
   }, [mines, flaggedCells]);
 
-  const handleCellHover = handleCellHoverFactory(setHoveredCell);
-  const handleCellLClick = handleCellLClickFactory(
-    mines, setMines, calcNewMines, 
-    revealedCells, setRevealedCells,
-    setIsGameOver, adjacentMines, adjacentCells
-  );
-  const handleCellDoubleClick = handleCellDoubleClickFactory(
-    mines, setMines, calcNewMines, 
-    revealedCells, setRevealedCells,
-    setIsGameOver, adjacentMines, adjacentCells,
-    flaggedCells
-  );
-  const handleCellRClick = handleCellRClickFactory(
-    revealedCells, flaggedCells, setFlaggedCells
-  )
+  /* handlers */
+  const handleCellHover = (id: number): React.MouseEventHandler<SVGSVGElement> => (e: React.MouseEvent<SVGSVGElement>) => {
+    setHoveredCell(id);
+  };
+
+  const handleCellLClick = (clickedCell: number): React.MouseEventHandler<SVGSVGElement> => (e: React.MouseEvent<SVGSVGElement>) => {
+    if (revealedCells.includes(clickedCell)) return;
+    revealCell(clickedCell);
+  }
+
+  const revealCell = (clickedCell: number) => {
+    if (mines.includes(clickedCell)) {
+      if (!revealedCells.length) {
+        let newMines = data ? _.sampleSize(_.range(data.length), data.length / mineRate) : [];
+        while (newMines.includes(clickedCell)) { 
+          // re-calculate mines until mines[] doesn't inlcude clickedCell
+          console.log("re-calc mines");
+          newMines = data ? _.sampleSize(_.range(data.length), data.length / mineRate) : [];
+        }
+        setMines(newMines);
+        setRevealedCells([clickedCell]);
+        return; 
+      }
+      else {
+        setIsGameOver(true);
+        return;
+      }
+    }
+    
+    if (adjacentMines[clickedCell]) {
+      // 주변에 지뢰가 있다면 그 셀만 밝히기
+      setRevealedCells([...revealedCells, clickedCell]);
+    }
+    else {
+      // 주변 지뢰가 없다면 BFS 
+      const queue = [clickedCell];
+      const visited = [clickedCell, ...revealedCells];
+      while (queue.length > 0) {
+        const currentCell: number | undefined = queue.shift();
+        if (currentCell === undefined) continue;
+        // 현재 셀에 인접한 셀 순회
+        for (const cell of adjacentCells[currentCell]) {
+          // 인접한 셀이 밝혀진 셀이 아니거나, 지뢰가 아니라면 밝혀질 셀 목록인 큐에 추가
+          if (!visited.includes(cell)) {
+            if (!adjacentMines[cell]){
+              queue.push(cell);              
+            }
+            visited.push(cell);
+          }
+        }
+      }
+      setRevealedCells([...visited]);
+    }
+  }
+
+  const handleCellDoubleClick = (id: number): React.MouseEventHandler<SVGSVGElement> => (e: React.MouseEvent<SVGSVGElement>) => {
+    if (!revealedCells.includes(id)) return;
+    const adjacentFlaggedCells = adjacentCells[id].filter((cellId) => flaggedCells.includes(cellId));
+    if (adjacentFlaggedCells.length === adjacentMines[id]) {
+      adjacentCells[id]
+        .filter((cellId) => !flaggedCells.includes(cellId))
+        .forEach((cellId) => {
+          revealCell(cellId);
+        })
+    }
+  }
+
+  const handleCellRClick = (id: number): React.MouseEventHandler<SVGSVGElement> => (e: React.MouseEvent<SVGSVGElement>) => {
+    e.preventDefault();
+    // 깃발이 있으면 빼고, 없으면 넣기
+    if (revealedCells.includes(id)) return; // 이미 밝혀진 셀이면 깃발 X
+    setFlaggedCells(flaggedCells.includes(id) ? flaggedCells.filter((cell) => (id !== cell)) : [...flaggedCells, id]);
+  }
+  /* handlers */
 
   return (
     <GameContext.Provider value={{
